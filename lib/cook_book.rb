282fd7cb6d76f7c4e4067ce72d24d8ced1bfdb89
Carson Jardine <carsonjardine@gmail.com>
2020-09-23 09:46:31 -0600

    Add code to pass tests

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,11 +1,19 @@
 class CookBook
   attr_reader :recipes
-  
+
   def initialize
     @recipes = []
   end
 
   def add_recipe(recipe)
     @recipes << recipe
+  end
+
+  def ingredients
+    @recipes.flat_map do |recipe|
+      recipe.ingredients.map do |ingredient|
+        ingredient.name
+      end
+    end.uniq  
   end
 end
