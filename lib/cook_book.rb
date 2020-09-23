59a78784e0c79a9ca50bed9f774048f845c1f601
Carson Jardine <carsonjardine@gmail.com>
2020-09-23 10:49:33 -0600

    Add code for date, WIP summary

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,8 +1,11 @@
+require 'date'
+
 class CookBook
-  attr_reader :recipes
+  attr_reader :recipes, :date
 
   def initialize
     @recipes = []
+    @date = Date.today.strftime("%m-%d-%Y")
   end
 
   def add_recipe(recipe)
@@ -20,6 +23,21 @@ class CookBook
   def highest_calorie_meal
     @recipes.max_by do |recipe|
       recipe.total_calories
-    end 
+    end
+  end
+
+  def summary
+    summary = Hash.new
+    @recipes.each do |recipe|
+      summary[:name][:details] ||= []
+      summary[:name] = recipe.name
+       summary[:details] = recipe.ingredients_required
+    # summary_hash = {:name => nil, :details => {}}
+    # @recipes.map.with_object do |recipe, hash|
+    #   require "pry"; binding.pry
+    #   summary_hash[:name] = recipe.name
+    #   summary_hash[:details] << recipe.ingredients_required
+    end
+    summary
   end
 end
