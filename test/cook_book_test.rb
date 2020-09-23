2e74a7b09e7768622ff1cf772d59c7a6b49872aa
Carson Jardine <carsonjardine@gmail.com>
2020-09-23 09:46:20 -0600

    Write test to find ingredients

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -1,10 +1,16 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/recipe'
+require './lib/ingredient'
 require './lib/cook_book'
 
 class CookBookTest < Minitest::Test
   def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
 
@@ -21,5 +27,22 @@ class CookBookTest < Minitest::Test
     @cookbook.add_recipe(@recipe1)
     @cookbook.add_recipe(@recipe2)
     assert_equal [@recipe1, @recipe2], @cookbook.recipes
+  end
+
+  def test_it_can_find_ingredients
+    assert_equal [], @cookbook.ingredients
+
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+
+    expected = ["Cheese", "Macaroni", "Ground Beef", "Bun"]
+    assert_equal expected, @cookbook.ingredients
   end
 end
