b2f5074e89755d61775fc86247841578ffa491f8
Carson Jardine <carsonjardine@gmail.com>
2020-09-23 09:28:44 -0600

    Create cook book test and setup

---
+++ test/cook_book_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/recipe'
+require './lib/cook_book'
+
+class CookBookTest < Minitest::Test
+  def setup
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
+
+    @cookbook = CookBook.new
+  end
+
+  def test_it_exists
+    assert_instance_of CookBook, @cookbook
+  end
+
+end
+
+
+# @cookbook.add_recipe(recipe1)
+
+# @cookbook.add_recipe(recipe2)
+
+# @cookbook.recipes
+# => [#<Recipe:0x00007faae69c9698...>, #<Recipe:0x00007faae692a110...>]
